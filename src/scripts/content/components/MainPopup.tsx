d09f4e619c6cab62540fd35eedd5813b86acb13b
Viet Hoang <ngviethoang0212@gmail.com>
2024-06-10 08:33:08 +0700

    update UI

--- src/scripts/content/components/MainPopup.tsx
+++ src/scripts/content/components/MainPopup.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import React, { useEffect } from 'react'
 
 export default function MainPopup() {
+  useEffect(() => {
+    console.log('Gimme: MainPopup.tsx')
+  }, [])
+
   return (
-    <div className="fixed top-0 right-0 w-[30vw] h-[calc(100vh_-_3.5rem)] p-5 rounded-xl border bg-card text-card-foreground shadow z-[2147483666] overflow-auto">
-      <h3 className="font-semibold leading-none tracking-tight">Welcome</h3>
+    <div className="fixed top-0 right-0 w-[30vw] h-[calc(100vh_-_3.5rem)] p-5 pr-0 rounded-xl border bg-card text-card-foreground shadow z-[2147483666] overflow-auto">
+      <ScrollArea className="h-full pr-5">
+        <h3 className="font-semibold leading-none tracking-tight">Welcome</h3>
+        <div className="mt-3">Content</div>
+      </ScrollArea>
     </div>
   )
 }
